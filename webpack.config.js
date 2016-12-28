82ae60abe74f79552fc0384ed7d2c7d6aba0c3f5
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-12-28 20:44:37 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: __dirname + '/index.js',
+    output: {
+        path: __dirname + '/dist', 
+        filename: '/bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['latest', 'env']
+                }
+            }
+        ]
+    },
+    stats: {
+        // Nice colored output
+        colors: true
+    }
+};
